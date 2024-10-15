72c24bb5e3948976aa4b4e39ba4e07b6cc1bd469
momadAB <mabaqer99@gmail.com>
2024-10-15 11:00:18 +0300

    Task 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import pastaImage from "./assets/pasta.jpg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+    <div className="grid items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
+      <h1>DeLuca's Italian Restaurant</h1>
+      <p>Authentic, beloved, and delicious.</p>
+      <Image src={pastaImage} width={500} height={500} alt="Pasta" />
+    </div>
   );
 }
