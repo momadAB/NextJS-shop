7be289511905b09d2a410e426c9386545f3b2096
momadAB <mabaqer99@gmail.com>
2024-10-15 13:23:22 +0300

    Polish

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,43 @@
 import Products from "./products";
 import Image from "next/image";
 import pastaImage from "./assets/pasta.jpg";
+import delucasLogoImage from "./assets/DeLucasLogo.png";
 
 export default function Home() {
   return (
-    <div className="grid items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
-      <h1>DeLuca's Italian Restaurant</h1>
-      <p>Authentic, beloved, and delicious.</p>
-      <Image src={pastaImage} width={500} height={500} alt="Pasta" />
+    <>
+      <div className="relative grid justify-items-center font-[family-name:var(--font-geist-sans)] h-[100vh] w-full">
+        {/* Background */}
+        <div
+          className="absolute top-0 left-0 h-full w-full"
+          style={{
+            backgroundImage: `url(${pastaImage.src})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            filter: "brightness(0.4)",
+          }}
+        ></div>
+
+        {/* Logo */}
+        <Image
+          className="absolute z-10 h-5/6 bottom-[-5rem] left-[-5rem]"
+          src={delucasLogoImage}
+          style={{
+            width: "auto",
+          }}
+        />
+
+        {/* Menu button */}
+        <button className="absolute z-20 h-44 w-36 bg-lime-800 hover:bg-lime-700 top-10 right-10 rounded-[50px] font-bold text-2xl">
+          MENU
+        </button>
+      </div>
+
       {/* Menu Area */}
-      <h1>Menu</h1>
-      <div className="flex justify-items-center gap-4">
+      <h1 className="text-center m-10 size">MENU</h1>
+      <div className="flex justify-center gap-4">
         <Products />
       </div>
-    </div>
+    </>
   );
 }
--- src/app/products.js
+++ src/app/products.js
@@ -10,15 +10,26 @@ const products = [
 
 export default function Products() {
   return (
-    <div className="flex gap-4 justify-center">
+    <div className="flex gap-4 justify-center h-80 wrap">
       {products.map((product, index) => (
-        <div key={index} className="flex flex-col items-center">
+        <div
+          key={index}
+          className="flex flex-col items-center w-56 overflow-hidden rounded-3xl border-2 justify-between pb-2"
+        >
           <Image
+            className="brightness-50"
             src={product.image}
             height={250}
-            width={250}
-            style={{ objectFit: "cover" }}
+            // width={250}
+            style={{
+              objectFit: "cover",
+              maxWidth: "none",
+              // width: 250,
+              // height: 250,
+              // flexShrink: 3,
+            }}
           />
+          <br />
           <p> {product.name} </p>
           <p className="text-xs"> {product.price} </p>
         </div>
